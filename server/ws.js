7b1b51d19aaf2920f6a6da3d00177100da4f19aa
luigi guerreros <luigi.guerreros@gmail.com>
2015-01-16 15:28:36 -0500

    subiendo proyecto

---
+++ server/ws.js
@@ -0,0 +1,15 @@
+module.exports = function (servidor, cache){
+  
+  var sio = require('socket.io');
+  var ws = sio.listen(servidor);
+
+  //ws.disable('log');
+  // websocket, htmlfile, xhr-polling, jsonp-polling
+  ws.on('connection', function (socket){
+    socket.emit('ready',{title: 'Ready'});
+    socket.on('imagen', function (imagen){
+      cache[imagen.id] = imagen.data;
+      socket.broadcast.emit('imagen', imagen);
+    });
+  });
+};
